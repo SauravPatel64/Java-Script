7adfab1a8190d74601528b3a5a1d62408f766d19
SauravPatel64 <sauravpatel220930@gmail.com>
2025-02-21 07:41:56 +0000

    container config done

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3); // we are using nodejs, not browser
+
+
+let name = "saurav"
+let age =18
+let isLoggedIn = false
+
+// Primitive Data type
+//number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+
+
+console.log(typeof null); // object
+console.log(typeof undefined);
+
