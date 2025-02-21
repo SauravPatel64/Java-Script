9db5e8902e3bdf7fa2d85dff5e9bbdec1f03e131
SauravPatel64 <sauravpatel220930@gmail.com>
2025-02-21 08:05:17 +0000

    container config done

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "18"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// 
+
+let isLoggedIn = "saurav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true;  0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = "33"
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
