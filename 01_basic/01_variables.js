fa7c1b325e8732abd91920884b3dfa22d49e6416
SauravPatel64 <sauravpatel220930@gmail.com>
2025-02-21 07:14:09 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "saurav220930@gmail.com"
+var accountPassword = "12345r"
+accountCity = " Jaipur"
+let accountState;
+
+// accountId = 2 
+
+accountEmail = "hd@hc"
+accountPassword = "21131232"
+accountCity="Bangaluru"
+
+console.log(accountId);
+/*
+
+
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
